834579cef9888fa2f3621a0a228ebca7a360a60e
MaiwennB <maiwenn.bizouarn@etu.univ-nantes.fr>
2017-12-12 09:25:57 +0100

    1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<HTML>
+    <HEAD>
+    </HEAD>
+    <BODY>
+            <INPUT type="button" onclick="color()" value="START/STOP"></INPUT>
+    </BODY>
+</HTML>
+<SCRIPT  type="text/javascript" >
+    function color(){
+        //alert("test");
+        
+        document.body.style.backgroundColor = "red" ;
+        
+    }
+</SCRIPT>
